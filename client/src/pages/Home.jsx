7717145820adc0595379629c8b0682ca7c27f5aa
akshatkatwala <arkatwala501@gmail.com>
2024-05-22 16:00:05 -0700

    Added location chatbox

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import "./home.css";
 
-export default function Home() {
+export default function Chat() {
+  const [data, setData] = useState({
+    chat: "",
+  });
+
+  const loginUser = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>Home</div>
-  )
+      <div class="chatbox">
+        <input
+          className="textchat"
+          placeholder="Type in future destination..."
+          type="text"
+          id="homeChat"
+          name="homeChat"
+          value={data.chat}
+          onChange={(e) => setData({ ...data, chat: e.target.value })}
+          required
+        />
+      </div>
+    
+  );
 }
